ee72cd1ce9faea8320663b9dd84b0700fa1f4eb8
Devansh Saxena <devanshsaxean1019@gmail.com>
2024-12-21 23:41:44 +0530

    Initial Commit

---
+++ Swiggy-iOS-UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiggy-iOS-UI
+//
+//  Created by Devansh Saxena on 21/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiggy-iOS-UI/Swiggy_iOS_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiggy_iOS_UIApp.swift
+//  Swiggy-iOS-UI
+//
+//  Created by Devansh Saxena on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiggy_iOS_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
